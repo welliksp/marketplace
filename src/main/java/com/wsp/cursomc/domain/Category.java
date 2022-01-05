ded875d2763b75ea7a79139cd0cea8a04db29ea1
Wellik Perroni <welliksp@gmail.com>
2022-01-05 09:17:12 -0300

    test first class category

---
+++ src/main/java/com/wsp/cursomc/domain/Category.java
@@ -0,0 +1,25 @@
+package com.wsp.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/wsp/cursomc/resources/CategoryResource.java
+++ src/main/java/com/wsp/cursomc/resources/CategoryResource.java
@@ -1,15 +1,33 @@
 package com.wsp.cursomc.resources;
 
+import com.wsp.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/category")
 public class CategoryResource {
 
     @GetMapping()
-    public String list(){
-        return "esta ok";
+    public List<Category> list(){
+        Category cat1 = new Category();
+        cat1.setId(1);
+        cat1.setName("Informatica");
+
+
+
+        Category cat2 = new Category();
+        cat2.setId(2);
+        cat2.setName("Escritorio");
+
+        List<Category> list = Arrays.asList(cat1,cat2);
+
+
+
+        return list;
     }
 }
